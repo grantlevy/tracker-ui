1753acbd43adf57361a5a33b4fcd7a35d23e60c7
Grant <grantlevy7@gmail.com>
2020-07-24 22:17:16 -0400

    update to page linked respository

--- src/Page.jsx
+++ src/Page.jsx
@@ -60,7 +60,7 @@ function Footer() {
       <p className="text-center">
         Full source code available at this
         {' '}
-        <a href="https://github.com/vasansr/pro-mern-stack-2">
+        <a href="https://github.ccs.neu.edu/NEU-CS5610-SU20/GrantLevy-Book">
           GitHub repository
         </a>
       </p>
